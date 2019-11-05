01b8bd3ddde911a264a80e8e3b186bbdca94e6b1
Hanna Lee <mani21na@gmail.com>
2019-11-05 04:29:06 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
+	#validates_presence_of :username, :password
 	
+	#def initialize(username, password)
+	#  @usename = username
+	#  @password = password
+	#end 
 end
